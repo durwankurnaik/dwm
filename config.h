a704f64c350ce32c7d001681b605dd658dcb180a
Durwankur Naik <ndurwankur14@gmail.com>
2021-12-04 23:51:17 +0530

    Some minor changes

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -12,8 +12,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int swallowfloating    = 0;        /* 1 means swallow floa    ting windows by default */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Jetbrains Mono:size=12", "Joypixels:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Jetbrains Mono:size=12";
+static const char *fonts[]          = { "Jetbrains Mono:size=11", "Joypixels:pixelsize=11:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Jetbrains Mono:size=11";
 
 #include "/home/sparrxw/.cache/wal/colors-wal-dwm.h"
 
